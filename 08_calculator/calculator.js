977a00572dc55b42c0f40fca44b6ef76621e09e9
Mario Slisko <marioslisko1@gmail.com>
2024-05-06 14:37:46 +0200

    solve exercise 8 - Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce((total, current) => (total + current), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((total, current) => (total * current), 1);
 };
 
-const power = function() {
-	
+const power = function(number, exponent) {
+	return number**exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0 || num === 1) {
+    return 1
+  }
+  else {
+    return num * factorial(num-1);
+  }
 };
 
 // Do not edit below this line
