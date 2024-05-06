d3618cc0675efd49b17f29c3265cffbecb14aed8
Mario Slisko <marioslisko1@gmail.com>
2024-05-06 16:04:16 +0200

    solve exercise 12 - FindTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,36 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const ages = []
+    for (let i = 0; i < people.length; i++) {
+        if (people[i].yearOfDeath === undefined) {
+            people[i].yearOfDeath = 2024;
+        }
+        ages.push(people[i].yearOfDeath - people[i].yearOfBirth);
+        //console.log(people[i].name);
+    }
+    indexOfOldestPerson = ages.indexOf(Math.max(...ages));
+    
+    return people[indexOfOldestPerson];
 };
 
+
+// const people = [
+//     {
+//         name: "Carly",
+//         yearOfBirth: 2018,
+//       },
+//       {
+//         name: "Ray",
+//         yearOfBirth: 1962,
+//         yearOfDeath: 2011,
+//       },
+//       {
+//         name: "Jane",
+//         yearOfBirth: 1912,
+//         yearOfDeath: 1941,
+//       },
+//   ]
+
+
+// findTheOldest(people);
 // Do not edit below this line
 module.exports = findTheOldest;
