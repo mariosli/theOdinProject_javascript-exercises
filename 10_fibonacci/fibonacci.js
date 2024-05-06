c9bbe24bfaeebc5472f7cd1ed4367191dfba0862
Mario Slisko <marioslisko1@gmail.com>
2024-05-06 15:29:54 +0200

    solve exercise 10 - Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    const fib_seq = [0];
+    for (let i = 0; i < num; i++) {
+        if (i === 0) {
+            fib_seq.push(fib_seq[i] + 1);
+        }
+        else {
+            fib_seq.push(fib_seq[i] + fib_seq[i-1]);
+        }  
+    }
+    return fib_seq[fib_seq.length-1];
 };
 
 // Do not edit below this line
