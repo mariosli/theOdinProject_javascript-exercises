c7f6eff37d8c67782a1b296d0ac7508854e95904
Mario Slisko <marioslisko1@gmail.com>
2024-05-06 15:02:32 +0200

    solve exercise 09 - Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (sentence) {
+    cleanSentence = sentence.replace(/[^A-Z0-9]/ig, '');
+    cleanSentence = cleanSentence.toLowerCase();
+    let reverseString = cleanSentence[cleanSentence.length - 1];
+    for (let i = cleanSentence.length-2; i >= 0; i--) {
+        reverseString = reverseString.concat(cleanSentence[i]);
+    }
+    return cleanSentence===reverseString ? true : false;
 };
 
 // Do not edit below this line
